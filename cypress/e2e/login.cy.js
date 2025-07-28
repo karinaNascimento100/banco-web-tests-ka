48e8dfc0d349780b2c880ed2b2a027492e2036fb
karinaNascimento100! <kalomeira9@gmail.com>
2025-07-28 15:44:09 -0300

    Inserido teste Cypress login invalido com inclusão de mensagem.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,9 +5,21 @@ describe('Login', () => {
 
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button', 'Entrar').click()
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
+  });
+
+  it('Login com dados inválidos deve exibir mensagem de erro', () => {
+    cy.visit('http://localhost:4000')
+
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('123466')
+    cy.contains('button', 'Entrar').click()
+
+    cy.get(".toast").should("be.visible")
+
+    cy.get(".toast").should("have.text", "Erro no login. Tente novamente.")
 
   });
 });
