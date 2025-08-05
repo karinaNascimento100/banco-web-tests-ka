8636555a6210ea7b5f0088a39e8b0a64444410bc
karinaNascimento100! <kalomeira9@gmail.com>
2025-08-05 09:05:48 -0300

    Adicionando testes no package.json.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,9 @@
 describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4000')
+  });
 
   it('Login com dados válidos deve permitir acesso no sistema', () => {
-    cy.visit('http://localhost:4000')
 
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
@@ -11,7 +13,6 @@ describe('Login', () => {
   });
 
   it('Login com dados inválidos deve exibir mensagem de erro', () => {
-    cy.visit('http://localhost:4000')
 
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123466')
